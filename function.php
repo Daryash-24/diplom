59328ca274a35226b531a5e475064b88decf2a68
Darya <dasha.dybalina1@gmail.com>
2026-03-22 03:01:27 -0800

    пока не робит

--- function.php
+++ function.php
@@ -1,18 +1,6 @@
 <?php
-function myvitrina_assets() {
-
-    wp_enqueue_style( 'myvitrina-style', get_stylesheet_uri(), array(), '1.0' );
+// Подключаем стили напрямую через wp_head
+function my_simple_styles() {
+    echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/style.css">';
 }
-
-add_action( 'wp_enqueue_scripts', 'myvitrina_assets' );
-
-// Поддержка миниатюр (изображений записей)
-add_theme_support( 'post-thumbnails' );
-
-// Регистрация меню
-function myvitrina_menus() {
-    register_nav_menus( array(
-        'primary' => 'Главное меню'
-    ) );
-}
-add_action( 'after_setup_theme', 'myvitrina_menus' );
+add_action('wp_head', 'my_simple_styles');
